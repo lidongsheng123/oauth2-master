2d982a4ba56046d2915ebb1224d7e882a9757978
李东升 <lidongsheng@bmsoft.com.cn>
2020-05-28 20:22:42 +0800

    页面元素引入 final0.3

---
+++ spring-security-web/src/main/java/com/oauth2/security/SecurityWebApp.java
@@ -0,0 +1,16 @@
+package com.oauth2.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：Mr.Li
+ * @date ：Created in 2020/5/28 15:47
+ */
+@SpringBootApplication
+public class SecurityWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityWebApp.class);
+    }
+}
